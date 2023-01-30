80a98f70fb1c7ba0d74b24e2a6c6cc7d8c156331
enes3774 <77508537+enes3774@users.noreply.github.com>
2023-01-30 12:35:36 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -34,27 +34,13 @@ python3 src/optimal_centers/get_optimal_centers.py \
     --data-prefix $CROPPED_IMAGE_PATH \
     --output-exam-list-path $EXAM_LIST_PATH \
     --num-processes $NUM_PROCESSES
-
-echo 'Stage 3: Generate Heatmaps'
-python3 src/heatmaps/run_producer.py \
-    --model-path $PATCH_MODEL_PATH \
-    --data-path $EXAM_LIST_PATH \
-    --image-path $CROPPED_IMAGE_PATH \
-    --batch-size $HEATMAP_BATCH_SIZE \
-    --output-heatmap-path $HEATMAPS_PATH \
-    --device-type $DEVICE_TYPE \
-    --gpu-number $GPU_NUMBER \
-    --batch-size 300
-
-
-echo 'Stage 4b: Run Classifier (Image+Heatmaps)'
+    
+echo 'Stage 4a: Run Classifier (Image)'
 python3 src/modeling/run_model.py \
-    --model-path $IMAGEHEATMAPS_MODEL_PATH \
+    --model-path $IMAGE_MODEL_PATH \
     --data-path $EXAM_LIST_PATH \
     --image-path $CROPPED_IMAGE_PATH \
-    --output-path $IMAGEHEATMAPS_PREDICTIONS_PATH \
-    --use-heatmaps \
-    --heatmaps-path $HEATMAPS_PATH \
+    --output-path $IMAGE_PREDICTIONS_PATH \
     --use-augmentation \
     --num-epochs $NUM_EPOCHS \
     --device-type $DEVICE_TYPE \
